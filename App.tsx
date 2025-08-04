9aabcd310a48e03bcee25a5836d480d30c896580
Mo Maaz <mohdmaazeesa5360@gmail.com>
2025-08-04 11:09:44 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,23 +1,23 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "@queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/toaster";
+import { TooltipProvider } from "@/tooltip";
 import Portfolio from "@/pages/portfolio";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Portfolio} />
-      <Route component={NotFound} />
+      <Route path="/" {Portfolio} />
+      <Route ={NotFound} />
     </Switch>
   );
 }
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider ={queryClient}>
       <TooltipProvider>
         <Toaster />
         <Router />
